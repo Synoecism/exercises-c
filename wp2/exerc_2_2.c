289a49575dd79795a88c874e6e5aead1837b9ba7
karlwes <karl.westgardh@gmail.com>
2020-01-22 16:58:08 +0100

    add print of integer and doubles

--- wp2/exerc_2_2.c
+++ wp2/exerc_2_2.c
@@ -39,9 +39,16 @@ int main()
     }
 
     printf("\n The value of the label array (address) is: %pn \n", &array);
-    printf("\n First integer in the array is (array[0]): %d \n", array[0]);
-    printf("\n The size of an integer (number of bytes) is: %ld \n", sizeof(array));
-    printf("\n The size of the whole array is: %d \n", n);
+    printf("First integer in the array is (array[0]): %d \n", array[0]);
+    printf("The size of an integer (number of bytes - array[0]) is: %lu \n", sizeof(array[0]));
+    printf("The size (length) of the whole array is: %d \n", n);
+    printf("The size (bytes) of the whole array is: %lu \n", sizeof(array));
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("Integer value of index %d %d \n", i, array[i]);
+        printf("Double value of index %d %lf \n", i, (double) array[i]);
+    }
 
     return 0;
 }
