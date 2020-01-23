26cd38995f5e91eac08eb29d496ab59b99ff6179
karlwes <karl.westgardh@gmail.com>
2020-01-23 10:59:50 +0100

    fix on top declarations in ex 2.1

--- wp2/exerc_2_1.c
+++ wp2/exerc_2_1.c
@@ -1,27 +1,27 @@
+// to read from txt instead of user input in terminal, use < myfile.txt after execution of gcc
+
 #include <stdio.h>
 #include <string.h>
 
-#define BUFFERSIZE 21 //Buffer is 21 to enable input of 20 char
+//Buffer is 21 to enable input of 20 char
+#define BUFFERSIZE 21 
 void copyString();
-
-// to read from txt instead of user input in terminal, use < myfile.txt after execution of gcc
 
 int main() {
 
     char from[BUFFERSIZE];
+    char copy[BUFFERSIZE];
+    char to[BUFFERSIZE];
 
     //wait for input
     fgets(from,BUFFERSIZE,stdin);
 
-    //SCENARIO A)
-    //copy of string (to,from)
-    char copy[BUFFERSIZE];
+    //SCENARIO A)    
     strcpy(copy,from);
 
     printf("\n Read and copied by use of strcpy() !: %s \n",copy);
 
     //SCENARIO B)
-    char to[BUFFERSIZE];
     copyString(&to,&from); //Passing the address of the variables and not their values
 
     printf("\n Read and copied by use of copyString() !: %s \n",to);
@@ -30,7 +30,6 @@ int main() {
 }
 
 void copyString(char *to, char *from){
-
     int len = strlen(from);
     
     for(int i = 0; i < len; i++){
