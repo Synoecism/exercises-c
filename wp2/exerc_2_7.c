478c1cdae87f00eaa17444356103b64330445efb
karlwes <karl.westgardh@gmail.com>
2020-01-23 15:35:19 +0100

    add fundament to ex. 2.7

--- wp2/exerc_2_7.c
+++ wp2/exerc_2_7.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+void readPersnr(char *person);
+int controlDigit(const char *persnr);
+
 int main() {
 
     return 0;
+}
+
+void readPersnr(char *person){
+
+}
+
+int controlDigit(const char *persnr){
+    
 }
