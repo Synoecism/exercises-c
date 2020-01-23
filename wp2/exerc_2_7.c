1383adba1ba6ee480cd277e9f167a9d7f5b4b666
karlwes <karl.westgardh@gmail.com>
2020-01-23 15:41:05 +0100

    remove exe files

--- wp2/exerc_2_7.c
+++ wp2/exerc_2_7.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void readPersnr(char *person);
 int controlDigit(const char *persnr);
+int personalNumber;
+char buf[1024]; // use 1KiB buffer just to be sure
 
-int main() {
+//Input checking from http://sekrit.de/webdocs/c/beginners-guide-away-from-scanf.html
 
-    return 0;
+int main(void) {
+
+    //The loop prevents undefined behaviour if the input is a char. Also prevents buffer overflow.
+    do
+    {
+        printf("Enter a personal number: \n");
+        if (!fgets(buf, 1024, stdin)) //reads input with fgets(), usidng standard input stdin. Checks we don't overflow buffer
+        {
+            // reading input failed, give up:
+            return 1;
+        }
+
+        personalNumber = atoi(buf); // Using the buffer and atoi ("Anything TO Integer")
+    } while (personalNumber != 0); // Returns 0 if the input was not a number
+
 }
 
 void readPersnr(char *person){
@@ -13,5 +30,5 @@ void readPersnr(char *person){
 }
 
 int controlDigit(const char *persnr){
-    
+
 }
