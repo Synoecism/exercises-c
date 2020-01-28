6494dede74a7bac5b8f45b1b518ba20ed2e68797
karlwes <karl.westgardh@gmail.com>
2020-01-28 14:52:39 +0100

    add skeleton to 3.4

--- wp3/exerc_3_4.c
+++ wp3/exerc_3_4.c
@@ -15,9 +15,34 @@ Important
 No code no exercise points!
 */
 
-#include <stdio.h>
+#include <stdlib.h>#include <stdio.h>
+// -----Typedefs -------//
+typedef struct
+{
+    char firstname[20];
+    char famnamne[20];
+    char pers_number[13];
+    yyyymmddnnnc
+} PERSON;
 
-int main() {
+// Function declaration (to be extend)
+PERSON input_record(void);
 
-    return 0;
-}
+// Reads in a person record.void
+write_new_file(PERSON *inrecord);
+
+//Creates a file and write a first recordvoid
+printfile(void);
+
+// print out all persons in the filevoid
+search_by_firstname(char *name);
+
+// print out person if in list void
+append_file(PERSON *inrecord);
+// appends a new person to the file
+
+int main(void)
+{
+    PERSON ppost;
+    return (0);
+}    
