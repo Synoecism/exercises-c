4eb55f781a22dc856715731cf127f34d17f8008f
NiklasMoller <niklasxmoller@gmail.com>
2020-01-29 12:18:16 +0100

    Gerenrating a doubly linked list with pointers

--- wp3/exerc_3_3.c
+++ wp3/exerc_3_3.c
@@ -54,6 +54,10 @@ int main(int argc, char *argv[])
     //create a random DLL and assign the head as the first node of the list (that is returned from the function)
     head = random_list();
 
+    printf("\n Printing the HEADS (aka TOP) number in main(): %d \n", head->number);
+    printf("\n Printing the next next from head in main(): %d \n", head->next->next->prev->prev->number);
+
+
     //set current position in DLL to the position of the head
     curr_position = head;
 
@@ -82,28 +86,58 @@ REGTYPE *random_list(void)
     int nr, i;
     i = 0;
 
+    REGTYPE *top, *old, *item;
+
+    //Init with NULL to set values in loop
+    old = NULL;
+    top = NULL;
+
+    REGTYPE *ptr_newNode;
+        
+
     //start loop to create DLL
     for (; i < MAX; i++)
     {
-        //create new node (head)
-        REGTYPE newNode;
+
+        //create new node
+        struct q *newNode;
+        newNode = (struct q *)malloc( sizeof(REGTYPE) );
+        printf("Address of newNode is: %x \n", &newNode);
 
         //create pointer to new node
-        REGTYPE *ptr_newNode = &newNode;
+        ptr_newNode = newNode;
+        printf("Address of ptr_newNode is: %x \n", ptr_newNode);
+
 
         //create random number between 0-100
         nr = (rand() % (UPPER - LOWER + 1)) + LOWER;
 
+        printf("The random number generated at iteration %d is:  %d \n", i, nr);
+
         //set value of node number
         ptr_newNode->number = nr;
 
-        //set pointer of node->next
+        if(i == 0){
+            top = ptr_newNode;    
+        }else{
+            old->next = ptr_newNode;
+        }
 
-        //set pointer of node->prev
-        ptr_newNode->prev = NULL;
+        printf("The value of old after else statement is: %p \n", old);
+
+        //set next to NULL
+        ptr_newNode->next = NULL;
+
+        ptr_newNode->prev = old; //Poiting to old as prev
+
+        old = ptr_newNode;
+
+        printf("The adress of ptr_newnode is: %p \n", ptr_newNode);
+        printf("The adress of old is: %p \n", old);
+        printf("The ptr_newnode - PREV: %p, NEXT: %p, NUM: %d \n", ptr_newNode->prev, ptr_newNode->next, ptr_newNode->number);
+        puts("\n");
+    
     }
-
-    REGTYPE *top, *old, *item;
 
     //return pointer to new node
     return (top);
