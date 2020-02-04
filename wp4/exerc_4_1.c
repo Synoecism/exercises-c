d0cc8120f0c7c51fa279e6170d53962922dec9e0
NiklasMoller <niklasxmoller@gmail.com>
2020-02-04 14:53:06 +0100

    Update with prints for logging

--- wp4/exerc_4_1.c
+++ wp4/exerc_4_1.c
@@ -15,6 +15,8 @@ Important
 No code no exercise points!
 */
 
+//Reference for HEX and Bytes: http://csweb.cs.wfu.edu/~turketwh/CSC101/Spring2011/BinaryHex.pdf
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -34,9 +36,22 @@ int main(int argc, char **argv)
         int brake2 = atoi(argv[5]);
 
         positions |= brake2; //Sets the Least Significant Bit (LSB), bit nr 0
+
+        //Prints to enhance understanding what happens in memory
+        //printf("The byte to hexadecimal after insert 1: %X \n", positions);
+
         positions |= (brake1 << 1); //Shifting to bit nr 1 
+
+        //printf("The byte to hexadecimal after insert 2: %X \n", positions);
+
         positions |= (key_pos << 2); //Shifting to bit nr 2
+
+        //printf("The byte to hexadecimal after insert 3: %X \n", positions);
+
         positions |= (gear_pos << 4); //Shifting to bit nr 4
+        
+        //printf("The byte to hexadecimal after insert 4: %X \n", positions);
+
         positions |= (engine_on << 7); //Shifting to bit nr 7
 
         printf("The byte to hexadecimal gives: %X \n", positions); // %X print out hexadecimal as upper case letters
