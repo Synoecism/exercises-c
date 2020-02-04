824c3c9d146781cfef2da0436a36a04abfda7894
karlwes <karl.westgardh@gmail.com>
2020-02-04 19:10:39 +0100

    add fundament to wp4 exercises

---
+++ wp4/exerc_4_3.c
@@ -0,0 +1,24 @@
+/*
+File name: exerc_4_3.c (or cpp)
+Date: 2020-02-02
+Group nr 1
+Members that contribute to the solutions
+Emanuel Dellsén
+Niklas Möller
+Karl Westgårdh 
+
+Member not present at demonstration time:
+NONE
+
+Demonstration code: []
+Important
+No code no exercise points!
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
---
+++ wp4/exerc_4_4.c
@@ -0,0 +1,25 @@
+/*
+File name: exerc_4_4.c (or cpp)
+Date: 2020-02-02
+Group nr 1
+Members that contribute to the solutions
+Emanuel Dellsén
+Niklas Möller
+Karl Westgårdh 
+
+Member not present at demonstration time:
+NONE
+
+Demonstration code: []
+Important
+No code no exercise points!
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
