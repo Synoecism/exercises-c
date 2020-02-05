512b043480f388a6068a9702b78367157d4d71fd
karlwes <karl.westgardh@gmail.com>
2020-02-05 11:31:33 +0100

    fix e4.2

--- wp4/exerc_4_2.c
+++ wp4/exerc_4_2.c
@@ -19,46 +19,118 @@ No code no exercise points!
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <math.h>
 
-//Unsigned char to store all the positions
-unsigned char positions;
+//inspiration: https://codeforwin.org/2015/09/c-program-to-convert-hexadecimal-to-decimal-number-system.html
+//reference: https://coolconversion.com/math/binary-octal-hexa-decimal/Convert_binary__10101011_to_hex_
+
+unsigned char unc;
+unsigned char nunc;
+char hex[17];
 
 int main(int argc, char **argv)
 {
     //execution command is one argument, therefore 2 = accepting 1 user argument
     if (argc == 2)
     {
-        //check if string is either one or two characters
-        if (strlen(argv[1]) < 2 && strlen(argv[1]) > 1)
+        int len = 0;
+        int i;
+        int val;
+        long long decimal;
+        int engine_on;
+        int gear_pos;
+        int key_pos;
+        int brake1;
+        int brake2;
+
+        //copy input to hex
+        strcpy(hex, argv[1]);
+
+        /* Find the length of total number of hex digit */
+        len = strlen(hex);
+        len--;
+
+        /*Iterate over each hex digit*/
+
+        for (i = 0; hex[i] != '\0'; i++)
         {
-            //check if string is hexadecimal value or not
-            if (isxdigit(argv[1]))
+
+            /* Find the decimal representation of hex[i] */
+            if (hex[i] >= '0' && hex[i] <= '9')
             {
-
-                //do some shit here do not know how it works
-
-                //Setting the different characters from the list of arguments as integers to prevent 'invalid operand to binary' error
-                int engine_on = atoi(argv[1]);
-                int gear_pos = atoi(argv[2]);
-                int key_pos = atoi(argv[3]);
-                int brake1 = atoi(argv[4]);
-                int brake2 = atoi(argv[5]);
-
+                //convert from ascii value to hexadecimal value
+                val = hex[i] - 48;
             }
-            else
+            else if (hex[i] >= 'a' && hex[i] <= 'f')
             {
-                puts("Not a hexadecimal value");
+                //convert from ascii value to hexadecimal value
+                val = hex[i] - 97 + 10;
             }
+            else if (hex[i] >= 'A' && hex[i] <= 'F')
+            {
+                //convert from ascii value to hexadecimal value
+                val = hex[i] - 65 + 10;
+            }
+            //conver from hexadecimal value to decimal value
+            decimal += val * pow(16, len);
+            len--;
         }
-        else
-        {
-            puts("Input should either be one or two charachters");
-        }
+
+        printf("Hexadecimal number = %s\n", hex);
+        printf("Decimal number = %lld\n", decimal);
+
+        //convert from long long to unsigned char
+        unc = (unsigned char)decimal;
+
+        printf("Byte of unc in hexadecimal: %X \n", unc);
+        printf("Byte of unc in decimal: %d \n", unc);
+
+        //get binary value of position 7 in byte
+        engine_on = ((unc >> 7) & 0x01);
+
+        //move value from unc pos 4 to nunc pos 0
+        nunc |= (((unc >> 4) & 0x01) << 0);
+
+        //move value from unc pos 5 to nunc pos 1
+        nunc |= (((unc >> 5) & 0x01) << 1);
+
+        //move value from unc pos 4 to nunc pos 0
+        nunc |= (((unc >> 6) & 0x01) << 2);
+
+        //assign the decimal value of unsigned char to gear_pos
+        gear_pos = (int)nunc;
+
+        //reset nunc
+        nunc = 0;
+
+        //move value from unc pos 2 to nunc pos 0
+        nunc |= (((unc >> 2) & 0x01) << 0);
+
+        //move value from unc pos 3 to nunc pos 0
+        nunc |= (((unc >> 3) & 0x01) << 1);
+
+        //assign the decimal value of unsigned char to gear_pos
+        key_pos = (int)nunc;
+
+        //get binary value of position 1 in byte
+        brake1 = ((unc >> 1) & 0x01);
+
+        //get binary value of position 1 in byte
+        brake2 = ((unc >> 0) & 0x01);
+
+        //print the result
+        printf("\n Name\t\tValue");
+        printf("\n-----------------------------");
+        printf("\nengine_on\t%d", engine_on);
+        printf("\ngear_pos\t%d", gear_pos);
+        printf("\nkey_pos\t\t%d", key_pos);
+        printf("\nbrake1\t\t%d", brake1);
+        printf("\nbrake2\t\t%d", brake2);
+        printf("\n");
     }
     else
     {
-        puts("Wrong number of arguments inserted. Exit program.");
+        puts("Wrong amount of arguments. Exiting program.");
     }
-
     return 0;
 }
