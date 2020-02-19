20f43f1a88350a21ac1f8a2224c879627640be1a
dellsen <dellsen@student.chalmers.se>
2020-02-19 14:20:48 +0100

    Add pseudocode for exerc 6_3

--- wp6/exerc_6_3.c
+++ wp6/exerc_6_3.c
@@ -16,8 +16,74 @@ No code no exercise points!
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <stdbool.h>
 
-int main() {
+#define ML13_Status 0x0B10
+#define ML13_Control 0x0B00 
 
-    return 0;
+void *doorSpaceSensorRunner(void *param);
+void *doorLockSensorRunner(void *param);
+
+void main () {
+
+bool doesTheDoorHaveElectricity = true;
+
+setup();
+
+
+
+while(doesTheDoorHaveElectricity){
+
+// if (pingFromDoorSpaceSensor)
+// set doorState to opening
+// start timer and countdown 7 seconds
+// when timer reach 7 seconds 
+// if(pingFromDoorLockSensor)
+// set doorState to open
+// start new counter to close door
+// if(pingFromDoorSpaceSensor)
+// reset timer to 7 seconds
+// when new counter reaches 7 seconds
+// set doorState to closing
+// if (pingFromDoorSpaceSensor)
+// set doorState to opening
+// else 
+// if(pingFromDoorLockSensor && new timer == 0 seconds)
+// set doorState to closed
+}
+
+}
+
+void setup(){
+
+
+
+pthread_t doorSpaceSensorThread_id;
+pthread_attr_t doorSpaceSensorThreadAttr;
+
+pthread_t doorLockSensorThread_id;
+pthread_attr_t doorLockSensorThreadAttr;
+
+
+pthread_attr_init(&doorSpaceSensorThreadAttr);
+pthread_create(&doorSpaceSensorThread_id, &doorSpaceSensorThreadAttr, doorSpaceSensorRunner, NULL);
+pthread_attr_init(&doorLockSensorThreadAttr);
+pthread_create(&doorLockSensorThread_id, &doorLockSensorThreadAttr, doorLockSensorRunner, NULL);
+
+// set door state to closed
+
+}
+
+void *doorSpaceSensorRunner(void *param){
+
+
+
+}
+
+void *doorLockSensorRunner(void *param){
+
+
+
 }
