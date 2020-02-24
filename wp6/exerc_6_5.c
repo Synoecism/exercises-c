54b8900637c26b4110595caf5bd82946bd521982
karlwes <karl.westgardh@gmail.com>
2020-02-24 10:49:15 +0100

    fix format of e 6.5

--- wp6/exerc_6_5.c
+++ wp6/exerc_6_5.c
@@ -16,60 +16,56 @@ No code no exercise points!
 */
 
 // Skeleton for exercise nr 5 in WP 6 course DIT632.
-// File skeleton_exerc_6_5.c  
+// File skeleton_exerc_6_5.c
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 
 pthread_mutex_t count_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t  not_empty = PTHREAD_COND_INITIALIZER;
-pthread_cond_t  not_full = PTHREAD_COND_INITIALIZER;
+pthread_cond_t not_empty = PTHREAD_COND_INITIALIZER;
+pthread_cond_t not_full = PTHREAD_COND_INITIALIZER;
 
 //**  Global buffer and corresponding counters. ** //
 
 // the starting letter
-char  letter = 'a';
+char letter = 'a';
 //buffer size
 #define MAX 10
 unsigned short count = 0;
 // circular buffer
-char buffer[MAX]; 
+char buffer[MAX];
 // index for next character to be put in buffer (put)
-int inpos = 0; 
+int inpos = 0;
 // index for next character to be read ( fetch )
-int outpos = 0; 
+int outpos = 0;
 
+// thread functions
 void *put();
 void *fetch();
 
-int main(){
-int i;
+int main()
+{
+    int i;
 
-   while(1){
-		....
-		....
-		
-   }
-
+    while (1)
+    {
+        // do something
+    }
 }
 
-
-void *put(){
-    while(1){
-		....
-		....
-		
+void *put()
+{
+    while (1)
+    {
+        //do something
     }
-   
 }
 
-
-void *fetch(){
-    while(1){
-		....
-		....
-		
+void *fetch()
+{
+    while (1)
+    {
+        // do something
     }
-
 }
