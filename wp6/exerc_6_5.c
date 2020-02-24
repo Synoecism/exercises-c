881481b5fe96862fedd1ace26d579dbadd9aaf33
karlwes <karl.westgardh@gmail.com>
2020-02-24 10:47:41 +0100

    add correct skeleton code to w 6.5

--- wp6/exerc_6_5.c
+++ wp6/exerc_6_5.c
@@ -15,79 +15,61 @@ Important
 No code no exercise points!
 */
 
-// Program for exercise 6.2alt in DIT632// Version 2020-02-12//
-//File : sortandfind_20.c// **************************************
+// Skeleton for exercise nr 5 in WP 6 course DIT632.
+// File skeleton_exerc_6_5.c  
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 
-#define num_elems 200
+pthread_mutex_t count_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t  not_empty = PTHREAD_COND_INITIALIZER;
+pthread_cond_t  not_full = PTHREAD_COND_INITIALIZER;
 
-int array[200];
-int max_value;
-void *runner(void *param);
+//**  Global buffer and corresponding counters. ** //
 
-int main()
-{
-    time_t t;
-    pthread_t tid;
-    pthread_attr_t attr;
-    srand((unsigned)time(&t));
-    pthread_attr_init(&attr);
-    printf("Enter max value\n");
-    scanf("%d", &max_value);
-    pthread_create(&tid, &attr, runner, NULL);
-    pthread_join(tid, NULL);
-    int c, d, swap;
-    for (c = 0; c < num_elems; c++)
-    {
-        for (d = 0; d < num_elems - c - 1; d++)
-        {
-            if (array[d] > array[d + 1])
-            {
-                swap = array[d];
-                array[d] = array[d + 1];
-                array[d + 1] = swap;
-            }
-        }
-    }
-    int search;
-    printf("Enter value to find\n");
-    scanf("%d", &search);
-    int first = 0;
-    int last = num_elems - 1;
-    int middle = (first + last) / 2;
-    int n;
-    for (n = 0; n < num_elems; n++)
-    {
-        printf("\n %d", array[n]);
-    }
-    while (first <= last)
-    {
-        if (array[middle] < search)
-            first = middle + 1;
-        else if (array[middle] == search)
-        {
-            printf("\n %d found at location %d.\n", search, middle + 1);
-            break;
-        }
-        else
-        {
-            last = middle - 1;
-            middle = (first + last) / 2;
-        }
-    }
-    if (first > last)
-        printf("\n Not found! %d is not present in the list.\n", search);
-    return 0;
+// the starting letter
+char  letter = 'a';
+//buffer size
+#define MAX 10
+unsigned short count = 0;
+// circular buffer
+char buffer[MAX]; 
+// index for next character to be put in buffer (put)
+int inpos = 0; 
+// index for next character to be read ( fetch )
+int outpos = 0; 
+
+void *put();
+void *fetch();
+
+int main(){
+int i;
+
+   while(1){
+		....
+		....
+		
+   }
+
 }
-void *runner(void *param)
-{
-    int c = 0, n = 0;
-    for (c = 0; c < num_elems; c++)
-    {
-        for (n = 0; n < 1000; n++)
-            ; // To delay a bit array[c] = rand() % max_value;}pthread_exit(0);
+
+
+void *put(){
+    while(1){
+		....
+		....
+		
     }
+   
+}
+
+
+void *fetch(){
+    while(1){
+		....
+		....
+		
+    }
+
 }
