6c048ff6d6285d9dfe390b58d3155cd5162155dc
karlwes <karl.westgardh@gmail.com>
2020-02-25 13:35:18 +0100

    add exam 3 file

--- exam/exam3.c
+++ exam/exam3.c
@@ -7,6 +7,36 @@
 #define MAX 7
 
 int *lotto_row(void);
+void sorting(int *tab);
+
+void sorting(int *tab)
+{
+    int current_min = 0;
+    int temp = 0;
+    int min_index = 0;
+
+    for (int i = 0; i < MAX-1; i++)
+    {
+        current_min = tab[i];
+        for (int j = i; j < MAX; j++)
+        {
+            if (tab[j] < current_min)
+            {
+                current_min = tab[j];
+                min_index = j;
+            }
+        }
+        temp = tab[i];
+        tab[i] = current_min;
+        tab[min_index] = temp;
+    }
+
+    // print the array
+    for (int i = 0; i < MAX; i++)
+    {
+        printf("Tab index %d has value %d \n", i, tab[i]);
+    }
+}
 
 int *lotto_row(void)
 {
@@ -44,7 +74,11 @@ int main(void)
         {
             printf("%d \n", ptr[i]);
         }
-    } else {
+    }
+    else
+    {
         puts("shit failed");
     }
+
+    sorting(ptr);
 }
