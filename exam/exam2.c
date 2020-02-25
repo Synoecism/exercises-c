63263fa447dca5a63b9a23775983bd57dcd81d10
karlwes <karl.westgardh@gmail.com>
2020-02-25 08:51:23 +0100

    remove github actions yml file

--- exam/exam2.c
+++ exam/exam2.c
@@ -4,5 +4,12 @@
 
 int main(void)
 {
+    int num = 1; // (1) init variable with value
+    int *num_ptr = &num; // (2) create pointer
+    printf("%d \n",*num_ptr); // (3) print value of pointer 
+    printf("%p \n",num_ptr); // (4) print address of pointer
+    printf("%p \n",&num_ptr); // (5) print address of pointer value
+    printf("%p \n",&num); // (6) print address of value, same as (4)
+    printf("%d \n", num); // (7) print value of variable, same as (3)
 
 }
